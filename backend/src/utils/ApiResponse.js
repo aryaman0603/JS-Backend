e74c35382f6b72ada707b6b258667615a84d198e
aryaman0603 <aryamansharma3319@gmail.com>
2025-01-14 00:48:02 +0530

    push

--- backend/src/utils/ApiResponse.js
+++ backend/src/utils/ApiResponse.js
@@ -3,6 +3,6 @@ class ApiRespnse {
         this.statusCode = statusCode
         this.data = data
         this.message = message
-        this.success = statuCode < 400
+        this.success = statusCode < 400
     }
 }
